b7dd9d4da5d1f47599d9f099e9633585b53d8b3e
pkbotzs <165777512+pkbotzs@users.noreply.github.com>
2024-04-03 15:22:36 -0400

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ STICKERS_IDS = ('CAACAgQAAxkBAAEK99dlfC7LDqnuwtGRkIoacot_dGC4zQACbg8AAuHqsVDaMQe
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/64764b9e6df9d2d1162c7.jpg')).split() #use a gif video 🫠
+PICS = (environ.get('PICS', 'https://graph.org/file/e37bfd42ba780fa540602.mp4')).split() #use a gif video 🫠
 NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/a27dc8fe434e6b846b0f8.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://te.legra.ph/file/6f55d902f9bf2d0afd4bb.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
